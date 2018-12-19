fdd10383a45bc4ffab532ee1c907498d6b38483d
edisket <realedison4@gmail.com>
2018-12-19 19:15:55 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using DemoLibrary;
+
+
+namespace AppDomainDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessRequestBatch();
+            GC.Collect();
+            Console.WriteLine("Completed the batch press enter to continue");
+            Console.ReadLine();
+        }
+
+        private static void ProcessRequestBatch() {
+            Stopwatch watch = new Stopwatch();
+            Processor demoProcessor = new Processor();
+            watch.Start();
+
+            for (int index = 0; index < 10000; index++)
+            {
+                int id = index % 5000;
+                Console.WriteLine(demoProcessor.ProcessRequest(id));
+
+            }
+
+            watch.Stop();
+            Console.WriteLine("The processing took {0} ms to complete...", watch.ElapsedMilliseconds);
+        }
+    }
+}
